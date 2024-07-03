37156aa28c4b07399736bb37044c1bb760449f42
fabian ulises <ulises.fab@outlook.com>
2024-07-02 23:22:31 -0700

    Updated app component. Added state for todos.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
+import React, { Component, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [todos, setTodos] = useState([]);
   return (
     <div className="App">
     </div>
   );
-}
-
+};
 export default App;
