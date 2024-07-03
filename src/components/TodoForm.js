828393654154a3610b15a0e07b4423bef04138c6
fabian ulises <ulises.fab@outlook.com>
2024-07-02 23:36:49 -0700

    Updated todoform. Added hanlder function for user input.

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -2,9 +2,14 @@ import React, { Component, useState } from 'react';
 const TodoForm = () => {
     // State
     const [input, setInput] = useState('');
+    // Handler function for userinput state
+    const handleInputChange = (e) => {
+        // Updating input state
+        setInput(e.target.value);
+    };
     return (
         <form>
-            <input type='text' placeholder='enter task' value={input} maxLength={60} required autoFocus />
+            <input type='text' placeholder='enter task' value={input} maxLength={60} required autoFocus onChange={handleInputChange} />
         </form>
     );
 };
