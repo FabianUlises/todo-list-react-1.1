b467ed6c259b3bcd9ba499955f463149041ce6a5
fabian ulises <ulises.fab@outlook.com>
2024-07-02 23:56:28 -0700

    Udpated app component. Adding todos to localstorage.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import React, { Component, useState } from 'react';
+import React, { Component, useEffect, useState } from 'react';
 import './App.css';
 import TodoForm from './components/TodoForm';
 
 function App() {
   // State
   const [todos, setTodos] = useState([]);
+  // useEffect
+  useEffect(() => {
+    // Adding todos to local storage
+    localStorage.setItem('todos', JSON.stringify(todos));
+  }, [todos]);
   return (
     <div className="App">
       <TodoForm setTodos={setTodos} />
