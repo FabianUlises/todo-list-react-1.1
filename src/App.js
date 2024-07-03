55ee9fcb7b5678b59c6af483c7a74d8c5425e110
fabian ulises <ulises.fab@outlook.com>
2024-07-02 23:27:03 -0700

    Created todoform component & rendering component on dom.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component, useState } from 'react';
 import './App.css';
+import TodoForm from './components/TodoForm';
 
 function App() {
   const [todos, setTodos] = useState([]);
   return (
     <div className="App">
+      <TodoForm />
     </div>
   );
 };
---
+++ src/components/TodoForm.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+const TodoForm = () => {
+    return (
+        <form>
+            <input type='text' />
+        </form>
+    );
+};
+export default TodoForm;
