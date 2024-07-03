638138c0b63a67d51f18ad95825194173c9dea8c
fabian ulises <ulises.fab@outlook.com>
2024-07-02 23:28:18 -0700

    Updated app component. Passing down setState to form component.

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import './App.css';
 import TodoForm from './components/TodoForm';
 
 function App() {
+  // State
   const [todos, setTodos] = useState([]);
   return (
     <div className="App">
-      <TodoForm />
+      <TodoForm setTodos={setTodos} />
     </div>
   );
 };
