e0169b5645c9f5f8e0eb680a93475926e4468c6c
fabian ulises <ulises.fab@outlook.com>
2024-07-02 23:46:29 -0700

    Updated todoform component. Created function to add new task to todo state.

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,5 +1,5 @@
 import React, { Component, useState } from 'react';
-const TodoForm = () => {
+const TodoForm = ({ setTodos }) => {
     // State
     const [input, setInput] = useState('');
     // Handler function for userinput state
@@ -7,8 +7,24 @@ const TodoForm = () => {
         // Updating input state
         setInput(e.target.value);
     };
+    // Handler function to update todos state using props
+    const addTodo = (todo) => {
+        // Not accepting empty string or spaces
+        if(todo === '' || /^\s*$/.test(todo)) return;
+        // Creating id for todo using js date function
+        const todoId = Date.now();
+        // Updating state
+        setTodos((currentTodos) => 
+            [...currentTodos, {
+                id: todoId,
+                task: todo,
+                completed: false,
+                isEditing: false
+            }]
+        );
+    };
     return (
-        <form>
+        <form onSubmit={handleSubmit}>
             <input type='text' placeholder='enter task' value={input} maxLength={60} required autoFocus onChange={handleInputChange} />
         </form>
     );
