2b4c4da49396cb4f09c032867554c7c03d9f04d1
fabian ulises <ulises.fab@outlook.com>
2024-07-03 20:30:06 -0700

    Updated listtoggle component. Passed down setStatus as props and updating state with user input.

--- src/components/ListToggle.js
+++ src/components/ListToggle.js
@@ -1,8 +1,13 @@
 import React from 'react'
 
-const ListToggle = () => {
+const ListToggle = ({ setStatus }) => {
+  // Hanlder function for select options
+  const handleChange = (e) => {
+    // Updating status state
+    setStatus(e.target.value);
+  };
   return (
-    <select name='todos'>
+    <select onChange={(e) => handleChange(e)} name='todos'>
         <option value='all'>All</option>
         <option value='completed'>Completed</option>
         <option value='uncompleted'>Uncompleted</option>
